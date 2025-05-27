ef3a762c7454943f92030b533dea2ccedfb2bd3c
Nuria Calo <nuriacalomosquera@gmail.com>
2025-05-27 10:19:51 +0200

    exercice 24 resolved

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,25 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
+        Queue<String> newQueue = new LinkedList<>();
+        newQueue.add("Smith");
+        newQueue.add("Montessori");
+        newQueue.add("Peralta");
+        newQueue.add("House");
 
+        return newQueue;
     }
-    public static void printAndEmptyQueue(Queue<String> queue) {
 
+    public static void printAndEmptyQueue(Queue<String> queue) {
+        for (String cola : queue) {
+            System.out.println(cola);
+        }
+        queue.clear();
     }
 
     public static void main(String[] args) {
-
+        Queue<String> myQueue = createQueue();
+        printAndEmptyQueue(myQueue);
     }
 
 }
